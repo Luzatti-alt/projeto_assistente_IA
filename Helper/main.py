cfb428d28ce8d1053c515074c7cc911eeea6599d
Luzatti-alt <lucasguzatti4@gmail.com>
2025-07-21 11:29:41 -0300

    Update main.py

--- Helper/main.py
+++ Helper/main.py
@@ -1,5 +1,8 @@
 from resp import Resp
+from tts import TTS
 if __name__ == "__main__":#roda somente se main está aberto
     print("iniciando helper")
     resp = Resp()  # Cria a instância da classe Resp
     resp.ia()
+    tts = TTS()
+    tts.rodar_tts()
