99c9d8553d5d28a1987cc96ce3ccfdb9846b7ffb
Luzatti-alt <lucasguzatti4@gmail.com>
2025-07-21 11:26:08 -0300

    Update resp.py

--- Helper/resp.py
+++ Helper/resp.py
@@ -1,5 +1,4 @@
 import google.generativeai as gemini
-from tts import TTS
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -26,14 +25,7 @@ class Resp:
             resposta = model.generate_content(prompt_2)
             # Exibir a resposta
             print("\nGemini responde:")
-            print(resposta.text)
-            # Instanciar a classe TTS e gerar a voz a partir da resposta
-            tts = TTS()
-            print("Passando a resposta para o TTS...")
-            tts.msg_voz(resposta.text)
-            # Executar o TTS para gerar o áudio
-            print("Iniciando o TTS...")
-            tts.rodar_tts()
+            print(resposta.text)#resposta.text num obj e e, tts ele consulata o resposta.text
         except Exception as e:
             print(f"Ocorreu um erro: {e}")
             print("Verifique sua chave de API, a quantidade de usos ou a disponibilidade do serviço.")
