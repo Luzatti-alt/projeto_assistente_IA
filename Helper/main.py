7a24a3b495e1f44febfe57cb868ccf07fd3e37c4
Luzatti-alt <lucasguzatti4@gmail.com>
2025-07-21 11:02:07 -0300

    Update main.py

--- Helper/main.py
+++ Helper/main.py
@@ -1,4 +1,5 @@
-from resp import *
+from resp import Resp
 if __name__ == "__main__":#roda somente se main está aberto
     print("iniciando helper")
+    resp = Resp()  # Cria a instância da classe Resp
     resp.ia()
